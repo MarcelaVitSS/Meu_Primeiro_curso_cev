7656434281ca350542e5dcf55d1630819a4f02aa
alvarodograu <159553463+MarcelaVitSS@users.noreply.github.com>
2024-03-15 11:51:02 -0300

    hellomyfriend
    nada

---
+++ desafio1.js
@@ -0,0 +1,9 @@
+const bonificacao = 1000;
+const pontos = 100;
+
+if(pontos>100){
+    console.log(`Parabéns,, sua pontuação foi maior que ${pontos}, então você ganhou uma bonificação de ${bonificacao}`)
+}
+else{
+    console.log(`Infelizmente a sua nota foi menor que ${pontos}, então você não ganhará uma bonificação`)
+}

18ec3ff2c69afbd815f0becf60d9916efbe43899
alvarodograu <159553463+MarcelaVitSS@users.noreply.github.com>
2024-03-15 11:47:44 -0300

    hellomyfriend
    kkkkkkkk

---
+++ index.js
@@ -0,0 +1,15 @@
+const numero1 = 7;
+const numero2 = 9;
+const media = (numero1+numero2)/2;
+
+const nomedoaluno = "marcela";
+
+console.log("a media entre os numeros é", media);
+console.log("O tipo de numero é", typeof numero1);
+
+if(media > 6){
+    console.log(`parabens, a nota foi ${media}`)
+}
+else{
+    console.log(`infelizmente sua nota foi ${media}, estude mais`)
+}

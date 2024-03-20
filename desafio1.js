378aa74ad8a147411de085bbc1d1fb59f370750b
alvarodograu <159553463+MarcelaVitSS@users.noreply.github.com>
2024-03-20 11:52:58 -0300

    Hellomyfriend
    Seja água, meu amigo,
    Be water, my friend.

--- desafio1.js
+++ desafio1.js
@@ -2,8 +2,8 @@ const bonificacao = 1000;
 const pontos = 190;
 
 if(pontos>100){
-    console.log(`Parabéns, sua pontuação foi maior que ${pontos}, então você ganhou uma bonificação de ${bonificacao}`)
+    console.log(`Parabéns, sua pontuação foi maior que 100, sendo de ${pontos} pontos, então você ganhou uma bonificação de ${bonificacao}`)
 }
 else{
-    console.log(`Infelizmente a sua nota foi menor que ${pontos}, então você não ganhará uma bonificação`)
+    console.log(`Infelizmente a sua nota foi menor que 100, sendo de ${pontos} pontos, então você não ganhará uma bonificação`)
 }

e61d6c37ce2f8f33d569d85014e1b7a5a547997d
alvarodograu <159553463+MarcelaVitSS@users.noreply.github.com>
2024-03-22 11:53:01 -0300

    Bewatermyfriend
    M

---
+++ desafio2.js
@@ -0,0 +1,11 @@
+const idade = 15
+
+if(idade>=18){
+    console.log(`Acesso permitido, você possui ${idade} anos, logo é maior de idade`)
+}
+if(idade=17){
+    console.log(`Acesso negado, você possui ${idade} ano, logo não é maior de idade, volte no próximo ano quando tiver 18 anos`)
+}
+else{
+    console.log(`Acesso negado, você possui ${idade} anos, logo não é maior de idade`)
+}

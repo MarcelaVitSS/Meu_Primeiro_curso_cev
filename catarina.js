c1b89d96cd649f4f0bc9af8ddf91236e6e5b6881
IzabeleHarlei <159553463+MarcelaVitSS@users.noreply.github.com>
2024-03-27 11:49:36 -0300

    catarina
    anacatarina

---
+++ catarina.js
@@ -0,0 +1,8 @@
+const catarina = {
+    materia: "matemática",
+    madalha: "OBA",
+    voluntariacao: "Hospital X",
+    escola: "Colégio CEV",
+    curso: "Programação",
+}
+console.log(catarina.escola)
